482d359af86411471f159c5688ab1756329d3e11
ciel <ciel@ciel-SR5B.3.1>
2024-10-18 14:06:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,9 +13,14 @@ int main() {
     const int HAUTEUR_GRILLE = 20;
     const int TAILLE_CELLULE = 30;
 
+    const int colonnes = 20;
+    const int lignes = 25;
+
     initPoints(); // Initialise les points
 
     Joueur joueur(TAILLE_CELLULE / 2, TAILLE_CELLULE / 2, TAILLE_CELLULE / 2);
+    dessinerGrille(HAUTEUR_GRILLE, LARGEUR_GRILLE, TAILLE_CELLULE);
+
 
     while (true) {
         // Gérer les entrées du clavier
@@ -25,7 +30,6 @@ int main() {
         verifierCollision(joueur);
 
         // Dessiner la grille et les points
-        dessinerGrille(HAUTEUR_GRILLE, LARGEUR_GRILLE, TAILLE_CELLULE);
         dessinerPoint(LARGEUR_GRILLE, HAUTEUR_GRILLE, TAILLE_CELLULE);
         
         // Dessiner le joueur
