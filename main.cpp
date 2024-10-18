536114e74689ae2d8c79003955f8dd6eda789fd8
ciel <ciel@ciel-SR5B.3.1>
2024-10-18 10:07:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <xamgraph.h>
 #include "grille.h"  // Inclusion du fichier grille.h
+#include "point.h" //inclusion du fichier point.h
 
 // Déclaration de l'instance de la classe XamGraph
-XamGraph graph(800, 600, "Pac-Man");
+XamGraph graph(800, 600, "Pac-Man"); // Taille de la fenetre de jeu 
 
 int main() {
     // Paramètres de la grille (20x20 cellules, chaque cellule fait 30 pixels)
     int lignes = 20;
-    int colonnes = 25;
+    int colonnes = 20;
     int tailleCellule = 30;
 
     // Dessiner la grille de base
     dessinerGrille(lignes, colonnes, tailleCellule);
+
+    // Dessiner les point
+    dessinerPoint(lignes, colonnes, tailleCellule);
 
     // Lancer la boucle d'événements
     graph.run();  // Appeler la méthode run() sur l'objet graph
