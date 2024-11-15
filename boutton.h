490db7cc2d9e87a3d75c18dfe4bf2438ea76ed0f
ahhhhh <ahhhhh>
2024-11-15 17:10:46 +0100

    Update boutton.h

--- boutton.h
+++ boutton.h
@@ -1,8 +1,11 @@
+#include <SDL2/SDL.h>
 #ifndef BOUTTON_H
 #define BOUTTON_H
 
 // Déclaration de la fonction de dessin du bouton
-void exit_boutton();
+void draw_exit_boutton();
+void exit_handler_button();
+void handle_mouse_click(SDL_Event& event);
 
 // Déclaration des constantes pour la taille du bouton
 extern const int TAILLE_HAUTEUR;
