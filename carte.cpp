ab5cc4520c4d579535a0c3be4863d8c39f7fb26d
ahhhhh <ahhhhh>
2024-12-06 16:56:44 +0100

    ADD carte.cpp

---
+++ carte.cpp
@@ -0,0 +1,36 @@
+#include "carte.h"
+#include <xamgraph.h>
+
+// Déclaration de la référence de l'instance de XamGraph
+extern XamGraph graph;
+
+// Définir la carte de jeu
+int carte[HAUTEUR_GRILLE][LARGEUR_GRILLE] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1},
+    {1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1},
+    {1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1},
+    {1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+    // Ajoutez plus de lignes pour compléter la carte
+};
+
+void initCarte() {
+    // Initialisation de la carte (si nécessaire)
+}
+
+void dessinerCarte(int tailleCellule) {
+    for (int y = 0; y < HAUTEUR_GRILLE; y++) {
+        for (int x = 0; x < LARGEUR_GRILLE; x++) {
+            if (carte[y][x] == 1) {
+                graph.setColor(XAM_BLUE); // Couleur des murs
+                graph.filledRectangle(x * tailleCellule, y * tailleCellule, tailleCellule, tailleCellule);
+            } else {
+                graph.setColor(XAM_BLACK); // Couleur des chemins
+                graph.filledRectangle(x * tailleCellule, y * tailleCellule, tailleCellule, tailleCellule);
+            }
+        }
+    }
+}
