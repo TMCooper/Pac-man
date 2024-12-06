2e0f99709413d8d253ef0cbf0918923e6d9f0c86
ahhhhh <ahhhhh>
2024-12-06 16:57:06 +0100

    Update joueur.cpp

--- joueur.cpp
+++ joueur.cpp
@@ -1,7 +1,8 @@
+#include "config.h"
 #include "joueur.h"
 #include <SDL2/SDL.h>
 #include <xamgraph.h>
-#include "grille.h" // Assure-toi que ce fichier contient les définitions
+#include "grille.h"
 
 // Déclaration de la référence de l'instance de XamGraph
 extern XamGraph graph;
@@ -15,15 +16,45 @@ void Joueur::dessiner() {
 }
 
 
+// void gererClavier(Joueur& joueur) {
+//     SDL_Event event;
+//     while (SDL_PollEvent(&event)) {
+//         if (event.type == SDL_KEYDOWN) {
+//             switch (event.key.keysym.sym) {
+//                 case SDLK_UP:    joueur.y -= joueur.taille*2; break;
+//                 case SDLK_DOWN:  joueur.y += joueur.taille*2; break;
+//                 case SDLK_LEFT:  joueur.x -= joueur.taille*2; break;
+//                 case SDLK_RIGHT: joueur.x += joueur.taille*2; break;
+//             }
+//         }
+//     }
+// }
+
 void gererClavier(Joueur& joueur) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         if (event.type == SDL_KEYDOWN) {
             switch (event.key.keysym.sym) {
-                case SDLK_UP:    joueur.y -= joueur.taille*2; break;
-                case SDLK_DOWN:  joueur.y += joueur.taille*2; break;
-                case SDLK_LEFT:  joueur.x -= joueur.taille*2; break;
-                case SDLK_RIGHT: joueur.x += joueur.taille*2; break;
+                case SDLK_UP:
+                    if (joueur.y - joueur.taille * 2 >= 0) {
+                        joueur.y -= joueur.taille * 2;
+                    }
+                    break;
+                case SDLK_DOWN:
+                    if (joueur.y + joueur.taille * 2 < HAUTEUR_GRILLE * TAILLE_CELLULE) {
+                        joueur.y += joueur.taille * 2;
+                    }
+                    break;
+                case SDLK_LEFT:
+                    if (joueur.x - joueur.taille * 2 >= 0) {
+                        joueur.x -= joueur.taille * 2;
+                    }
+                    break;
+                case SDLK_RIGHT:
+                    if (joueur.x + joueur.taille * 2 < LARGEUR_GRILLE * TAILLE_CELLULE) {
+                        joueur.x += joueur.taille * 2;
+                    }
+                    break;
             }
         }
     }
