17ca692544fe34780517ae524cdb6a805e818219
ahhhhh <ahhhhh>
2024-12-06 16:56:59 +0100

    Update Config.h

--- config.h
+++ config.h
@@ -3,5 +3,6 @@
 
 const int LARGEUR_GRILLE = 25; // Définir la largeur ici
 const int HAUTEUR_GRILLE = 20;  // Définir la hauteur ici
+const int TAILLE_CELLULE = 30; // Définir la taille de la cellule ici
 
 #endif // CONFIG_H
