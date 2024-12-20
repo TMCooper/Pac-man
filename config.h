100b1a182d0db40b9cde762e56ec1fc5038f6cd0
ahhhhh <ahhhhh>
2024-12-20 13:34:01 +0100

    ADD comment

--- config.h
+++ config.h
@@ -1,8 +1,13 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const int LARGEUR_GRILLE = 25; // Définir la largeur ici
-const int HAUTEUR_GRILLE = 20;  // Définir la hauteur ici
-const int TAILLE_CELLULE = 30; // Définir la taille de la cellule ici
+// Définir la largeur de la grille
+const int LARGEUR_GRILLE = 25;
+
+// Définir la hauteur de la grille
+const int HAUTEUR_GRILLE = 20;
+
+// Définir la taille de chaque cellule de la grille
+const int TAILLE_CELLULE = 30;
 
 #endif // CONFIG_H
