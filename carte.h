41e1dd782c5a84842ebba793079b513dd5bcaa59
ahhhhh <ahhhhh>
2024-12-20 13:33:58 +0100

    ADD comment

--- carte.h
+++ carte.h
@@ -3,9 +3,13 @@
 
 #include "config.h"
 
+// Déclaration de la carte de jeu
 extern int carte[HAUTEUR_GRILLE][LARGEUR_GRILLE];
 
+// Fonction pour initialiser la carte
 void initCarte();
+
+// Fonction pour dessiner la carte
 void dessinerCarte(int tailleCellule);
 
 #endif // CARTE_H
